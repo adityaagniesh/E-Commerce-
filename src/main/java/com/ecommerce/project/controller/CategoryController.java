e797c3214626de9d8c3b5ec89e1acb4240ca9234
aditya-agniesh <aditya.agniesh@gmail.com>
2025-08-09 00:25:39 +0530

    controler cleanup no manual try cath in category controller all handled by global exception handler using @valid

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -32,26 +31,16 @@ public class CategoryController {
     }
 
     @DeleteMapping("/admin/categories/{categoryId}")
-    public ResponseEntity<String> deleteCategory(@PathVariable  Long categoryId){
-        try{
+    public ResponseEntity<String> deleteCategory( @PathVariable  Long categoryId){
             String status=categoryService.deleteCategory(categoryId);
             return new ResponseEntity<>(status, HttpStatus.OK);
-        } catch (ResponseStatusException e){
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
     }
 
     @PutMapping("/admin/categories/{categoryId}")
-    public ResponseEntity<String> updateCategory(@RequestBody Category category,
+    public ResponseEntity<String> updateCategory(@Valid @RequestBody Category category,
                                                  @PathVariable Long categoryId){
-        try{
             Category savedCategory = categoryService.updateCategory(category, categoryId);
             return new ResponseEntity<>("Updated Category with category id : " + categoryId, HttpStatus.OK);
-        } catch(ResponseStatusException e){
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
     }
-
-
 
 }
