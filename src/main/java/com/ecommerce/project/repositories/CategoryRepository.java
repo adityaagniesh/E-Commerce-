296d6cc4c3138729c98e321dbf46cec54c74d059
aditya-agniesh <aditya.agniesh@gmail.com>
2025-08-08 23:56:49 +0530

    custom API Exception for remove duplicate in category

--- src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
+++ src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
@@ -1,9 +1,12 @@
 package com.ecommerce.project.repositories;
 
 import com.ecommerce.project.model.Category;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-                                                         //<type of entity, type of primary key of entity>
+//<type of entity, type of primary key of entity>
 public interface CategoryRepository extends JpaRepository<Category,Long> {
 
+    Category findByCategoryName(String categoryName);
 }
