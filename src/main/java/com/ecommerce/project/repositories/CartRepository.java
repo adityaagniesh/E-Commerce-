6643e25876e3fb04dae922ef10b9e1a033606c76
aditya-agniesh <aditya.agniesh@gmail.com>
2025-09-07 17:29:42 +0530

    adding product to cart

--- src/main/java/com/ecommerce/project/repositories/CartRepository.java
+++ src/main/java/com/ecommerce/project/repositories/CartRepository.java
@@ -2,6 +2,13 @@ package com.ecommerce.project.repositories;
 
 import com.ecommerce.project.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    // we are writing query because jpa doest not work on nested class
+    // here cart-> user -> email so we are writing query manually
+    // ?1 check for 1st parameter
+    @Query("SELECT c FROM Cart c WHERE c.user.email = ?1")
+    Cart findCartByEmail(String email);
 }
