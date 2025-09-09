b7df9d0b57d1bd4a5484ecf0f7708158c5e13311
aditya-agniesh <aditya.agniesh@gmail.com>
2025-09-09 22:20:53 +0530

    Fetching user cart

--- src/main/java/com/ecommerce/project/repositories/CartRepository.java
+++ src/main/java/com/ecommerce/project/repositories/CartRepository.java
@@ -11,4 +11,7 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
     // ?1 check for 1st parameter
     @Query("SELECT c FROM Cart c WHERE c.user.email = ?1")
     Cart findCartByEmail(String email);
+
+    @Query("SELECT c FROM Cart c WHERE c.user.email = ?1 AND c.id = ?2")
+    Cart findCartByEmailAndCartId(String emailId, Long cartId);
 }
