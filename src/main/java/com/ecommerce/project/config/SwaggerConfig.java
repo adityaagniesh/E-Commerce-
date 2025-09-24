aa34ef1679e407f9df4a70c7680d68d66b150a2a
aditya-agniesh <aditya.agniesh@gmail.com>
2025-09-24 14:50:40 +0530

    customizing Swagger Documentation

--- src/main/java/com/ecommerce/project/config/SwaggerConfig.java
+++ src/main/java/com/ecommerce/project/config/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package com.ecommerce.project.config;
 
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -22,6 +26,22 @@ public class SwaggerConfig {
                 .addList("Bearer Authentication");
 
         return new OpenAPI()
+                .info(new Info()
+                        .title("Spring Boot eCommerce API")
+                        .version("1.0")
+                        .description("This is a Spring Boot Project for eCommerce")
+                        .license(new License().name("Apache 2.0").url("http://ecommerce.com"))
+                        .contact(new Contact()
+                                .name("Aditya Agniesh")
+                                .email("adityaagniesh.cse@gmail.com")
+                                .url("https://github.com/aditya_agniesh")
+                        )
+                )
+                .externalDocs(new ExternalDocumentation()
+                        .description("Project Documentation")
+                        .url("http://ecommerce.com")
+                )
+
                 .components(new Components()
                         .addSecuritySchemes("Bearer Authentication", bearerScheme))
                 .addSecurityItem(bearerRequirement);
